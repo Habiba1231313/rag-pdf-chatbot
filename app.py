1d16c76018e3f0d4466c343e03e2edce5d54cd2d
hm2308043 <yashabiba23@gamil.com>
2026-01-08 02:40:52 +0300

    Extract and preview PDF text from first page

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import streamlit as st
+import pypdf as pdf
 
 st.title("RAG PDF Chatbot")
 st.text("Upload a PDF and chat with its content!")
-file =st.file_uploader("Upload your PDF file", type=["pdf"])
-st.write("thanks for uploading ", file.name if file else "No file uploaded yet.")
+file_object =st.file_uploader("Upload your PDF file", type=["pdf"])
+st.write("thanks for uploading ", file_object.name if file_object else "No file uploaded yet.")
 question = st.text_input("Ask a question about the PDF content:")
 st.button("Submit")
 
-
-
+if file_object is not None:
+    reader = pdf.PdfReader(file_object)
+    st.write("this file has ", len(reader.pages), " pages.")
+    for i in range(len(reader.pages)):
+        page_text = reader.pages[i].extract_text()
+        st.write(page_text[:300])
+else:
+    st.write("No file uploaded yet.")
 
