8503a625daa14a2012fa7a47b3fd6038e2e0634f
hm2308043 <yashabiba23@gamil.com>
2026-01-08 02:22:39 +0300

    Basic UI: upload + question input

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 import streamlit as st
-st.write("Hello, Streamlit!")
+
+st.title("RAG PDF Chatbot")
+st.text("Upload a PDF and chat with its content!")
+file =st.file_uploader("Upload your PDF file", type=["pdf"])
+st.write("thanks for uploading ", file.name if file else "No file uploaded yet.")
+question = st.text_input("Ask a question about the PDF content:")
+st.button("Submit")
+
+
+
+
